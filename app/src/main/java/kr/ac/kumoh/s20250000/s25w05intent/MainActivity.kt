8f4df75db5f01ec82e101924b67737317b1efde6
BWOH <bwoh@outlook.kr>
2025-09-30 22:37:36 +0900

    🔨LazyColumn 사용

--- app/src/main/java/kr/ac/kumoh/s20250000/s25w05intent/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20250000/s25w05intent/MainActivity.kt
@@ -5,16 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import kr.ac.kumoh.s20250000.s25w05intent.ui.theme.S25W05IntentTheme
 
@@ -37,18 +40,34 @@ fun MainScreen() {
         "인스타그램" to "https://www.instagram.com/blackpinkofficial/",
         "페이스북" to "https://www.facebook.com/bangtan.official",
         "우리집 좌표" to "geo:36.145014,128.393047?z=17",
-        "우리집 주소" to "geo:0,0?q=금오공과대학교",
+        "우리집 주소" to "geo:0,0?q=국립금오공과대학교",
         "전화" to "tel:054-478-7114",
-        "문자" to "sms:054-478-7114"
+        "문자" to "sms:054-478-7114",
+        "유튜브" to "https://www.youtube.com/results?search_query=android+developers",
+        "인스타그램" to "https://www.instagram.com/blackpinkofficial/",
+        "페이스북" to "https://www.facebook.com/bangtan.official",
+        "우리집 좌표" to "geo:36.145014,128.393047?z=17",
+        "우리집 주소" to "geo:0,0?q=국립금오공과대학교",
+        "전화" to "tel:054-478-7114",
+        "문자" to "sms:054-478-7114",
+        "유튜브" to "https://www.youtube.com/results?search_query=android+developers",
+        "인스타그램" to "https://www.instagram.com/blackpinkofficial/",
+        "페이스북" to "https://www.facebook.com/bangtan.official",
+        "우리집 좌표" to "geo:36.145014,128.393047?z=17",
+        "우리집 주소" to "geo:0,0?q=국립금오공과대학교",
+        "전화" to "tel:054-478-7114",
+        "문자" to "sms:054-478-7114",
+
     )
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            intentItems.forEach { item ->
+            items(intentItems) { item ->
                 IntentButton(item = item)
             }
         }
