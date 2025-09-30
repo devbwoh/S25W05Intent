57136819c1411a3bf4a97528257dea0515e47017
BWOH <bwoh@outlook.kr>
2025-09-30 21:25:13 +0900

    🎉Project 시작

---
+++ app/src/main/java/kr/ac/kumoh/s20250000/s25w05intent/MainActivity.kt
@@ -0,0 +1,37 @@
+package kr.ac.kumoh.s20250000.s25w05intent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kr.ac.kumoh.s20250000.s25w05intent.ui.theme.S25W05IntentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S25W05IntentTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+        }
+    }
+}
