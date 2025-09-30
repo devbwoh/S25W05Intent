d972aa39380de4e7aef25e47ae684cb2a9ec35ec
BWOH <bwoh@outlook.kr>
2025-09-30 21:50:42 +0900

    💩Intent 버튼들

--- app/src/main/java/kr/ac/kumoh/s20250000/s25w05intent/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20250000/s25w05intent/MainActivity.kt
@@ -1,15 +1,21 @@
 package kr.ac.kumoh.s20250000.s25w05intent
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.net.toUri
 import kr.ac.kumoh.s20250000.s25w05intent.ui.theme.S25W05IntentTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +38,44 @@ fun MainScreen() {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
+            IntentYouTube()
+            IntentHomepage()
         }
+    }
+}
+
+@Composable
+fun IntentYouTube(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    Button(
+        modifier = modifier
+            .fillMaxWidth(),
+        onClick = {
+            val uri = "https://www.youtube.com/results?search_query=android+developers".toUri()
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            context.startActivity(intent)
+        }
+    ) {
+        Text("유튜브")
+    }
+}
+
+@Composable
+fun IntentHomepage(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    Button(
+        modifier = modifier
+            .fillMaxWidth(),
+        onClick = {
+            //val uri = "https://www.facebook.com/[본인 계정]".toUri()
+            //val uri = "https://bwoh.github.io".toUri()
+            val uri = "https://www.facebook.com/bangtan.official".toUri()
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            context.startActivity(intent)
+        }
+    ) {
+        Text("홈 페이지")
     }
 }
